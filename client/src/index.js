ca30e588c13e92a9c0fee75e566c4903505bf278
Wahome <vinwahome@gmail.com>
2023-12-28 16:15:25 +0300

    Add some routes

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Newproduct from './pages/Newproduct';
+import Signup from './pages/Signup';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
+        <Route index element={<Home />} />
+        <Route path='menu' element={<Menu />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='login' element={<Login />} />
+        <Route path='newproduct' element={<Newproduct />} />
+        <Route path='signup' element={<Signup />} />
 
     </Route>
   )
