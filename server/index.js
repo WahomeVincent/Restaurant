9149e42d088340e69933f1cfea5872360bc42df9
Wahome <vinwahome@gmail.com>
2023-12-30 09:34:28 +0300

    Initial commit for the server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running at port:" + PORT);
+})
