4c22494eedc6f50f06f180d4e1c2ebaa3b61f7e6
Wahome <vinwahome@gmail.com>
2023-12-30 15:48:00 +0300

    Link the frontend and the backend

--- server/index.js
+++ server/index.js
@@ -4,11 +4,16 @@ const mongoose = require('mongoose')
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.send('Server is running')
+})
+
+app.post('/signup', (req,res) => {
+    console.log(req.body);
 })
 
 app.listen(PORT, () => {
