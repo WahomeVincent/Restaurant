1aa58631022907fcfde536c9153363a13d4f1de7
Wahome <vinwahome@gmail.com>
2024-01-10 22:13:33 +0300

    Work on the new product page

--- client/src/pages/Newproduct.js
+++ client/src/pages/Newproduct.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Newproduct() {
   return (
-    <div>Newproduct</div>
+    <div className='m-auto w-full max-w-md p-4'>
+      <form>
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' />
+
+        <select>
+          <option>Fruits</option>
+          <option>Vegetable</option>
+          <option>IceCream</option>
+          <option>Dosa</option>
+          <option>Pizza</option>
+        </select>
+
+        <div className='h-7 w-full bg-slate-500'>
+
+        </div>
+      </form>
+    </div>
   )
 }
 
