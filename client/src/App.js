04dd0f40e331b978aa5da73144830c730091bb35
Wahome <vinwahome@gmail.com>
2024-01-14 20:22:41 +0300

    Fetch data to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    (async() => {
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/products`)
+      const resData = await res.json()
+      console.log(resData);
+    })()
+  },[])
+
   return (
     <div className="App">
-       <Header />
-       <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+      <Toaster />
+       <Header/>
+       <main className='pt-16 bg-slate-100 min-h[calc(100vh)'>
           <Outlet /> 
        </main>
     </div>
