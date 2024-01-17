777bfbeca20c57e0df120dc1fa121866111e8a72
Wahome <vinwahome@gmail.com>
2024-01-17 20:02:53 +0300

    Basic layout of the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import Homecard from '../components/Homecard'
+import { useSelector } from 'react-redux'
 
 function Home() {
+  const productData = useSelector((state) => state.product.productList)
+  console.log(productData);
+  const homeProductCartList = productData.slice(0, 6)
   return (
-    <div>Home</div>
+    <div className='p-2 md:p-4'>
+      <div className='md:flex gap-4 py-2'>
+
+        <div className='md:w-1/2 '>
+          <div className='flex gap-3 items-center bg-blue-400 w-40 px-2 rounded-full font-medium text-white'>
+              <p className='text-sm '>Bike Delivery</p>
+              <img src='https://cdn-icons-png.flaticon.com/512/2972/2972185.png' alt='' className='h-7'/>
+          </div>
+          <h2 className='text-4xl md:text-7xl font-bold py-3'>The Fastest Delivery in <span className='text-blue-900'>Your Home</span> </h2>
+          <p className='py-3 text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <button className='font-bold bg-yellow-400 text-white px-2 py-2 rounded-lg'>Order Now</button>
+        </div>
+
+        <div className='md:w-1/2 flex flex-wrap gap-5 justify-center'>
+          {
+            homeProductCartList.map(item => {
+              return(
+                <Homecard
+                  image = {item.image}
+                  name = {item.name}
+                  price = {item.price}
+                  category = {item.category}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
