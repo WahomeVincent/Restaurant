a925358b28daa45bee26d23cbc508ad81dc5e166
Wahome <vinwahome@gmail.com>
2024-01-19 13:30:23 +0300

    Add a message when waiting for the data to load

--- client/src/components/Homecard.js
+++ client/src/components/Homecard.js
@@ -1,14 +1,25 @@
 import React from 'react'
 
-function Homecard({name, image, category, price}) {
+function Homecard({name, image, category, price, loading}) {
   return (
-    <div className='bg-white shadow-md p-2 '>
-        <div className='w-40 h-40 '>
+    <div className='bg-white shadow-md p-2 rounded w-44 min-h-[150px]'>
+      
+      {
+        name ? <>
+         <div className='w-40 h-40 '>
             <img src={image} alt='' className='w-full h-full'/>
         </div>
         <h2 className='text-center font-semibold pt-2'>{name}</h2>
         <p className='text-center text-slate-400'>{category}</p>
         <p className='text-center'><span className='text-blue-700'>Kes</span> <span>{price}</span></p>
+ 
+        </>
+        :
+        <div className='flex justify-center items-center h-full'>
+            <p>{loading} </p>
+        </div>
+      }
+       
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Homecard from '../components/Homecard'
 import { useSelector } from 'react-redux'
+import CardFeature from '../components/CardFeature';
 
 function Home() {
   const productData = useSelector((state) => state.product.productList)
-  console.log(productData);
   const homeProductCartList = productData.slice(0, 6)
+  const homeProductcartListVegetable = productData.filter(item => item.category === 'Vegetable')
+  console.log(homeProductcartListVegetable);
+
+  const loadingArray = new Array(6).fill(null)
+
   return (
     <div className='p-2 md:p-4'>
       <div className='md:flex gap-4 py-2'>
@@ -16,15 +21,17 @@ function Home() {
               <img src='https://cdn-icons-png.flaticon.com/512/2972/2972185.png' alt='' className='h-7'/>
           </div>
           <h2 className='text-4xl md:text-7xl font-bold py-3'>The Fastest Delivery in <span className='text-blue-900'>Your Home</span> </h2>
-          <p className='py-3 text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className='py-3 text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
           <button className='font-bold bg-yellow-400 text-white px-2 py-2 rounded-lg'>Order Now</button>
         </div>
 
         <div className='md:w-1/2 flex flex-wrap gap-5 justify-center'>
           {
+            homeProductCartList[0]?
             homeProductCartList.map(item => {
               return(
                 <Homecard
+                  key={item._id}
                   image = {item.image}
                   name = {item.name}
                   price = {item.price}
@@ -32,8 +39,32 @@ function Home() {
                 />
               )
             })
+            : loadingArray.map(item => {
+              return(
+                <Homecard 
+                  loading={'Loading...'}
+                />
+              )
+            })
           }
         </div>
+      </div>
+      <div className=''>
+            <h2 className='font-bold text-2xl text-slate-800 '>Fresh Vegetables</h2>
+            <div className='flex'>
+              { homeProductcartListVegetable.map(item => {
+                return(
+                  <CardFeature 
+                    key={item._id}
+                    image = {item.image}
+                    name = {item.name}
+                    price = {item.price}
+                    category = {item.category}
+                  />
+                )
+              })
+              }
+            </div>
       </div>
     </div>
   )
