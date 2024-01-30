9b3e1f8ed18d94bb38d13f3d2270bc14f191f817
Wahome <vinwahome@gmail.com>
2024-01-30 08:22:20 +0300

    Ensure that only one product of the same type is added to cart

--- client/src/redux/productSlice.js
+++ client/src/redux/productSlice.js
@@ -14,12 +14,22 @@ export const productSlice = createSlice({
                 state.productList = [...action.payload]
         },
         addCartItem : (state, action) => {
-                    const total = action.payload.price
-                    state.cartItem = [...state.cartItem, {...action.payload, quantity : 1, total : total}]
-        },
+
+                    const check = state.cartItem.some(item => item._id === action.payload._id)
+                    console.log(check);
+                    if (!check) {
+                        const total = action.payload.price
+                        const index = state.cartItem.findIndex(item => item._id === action.payload)
+                        state.cartItem = [...state.cartItem, {...action.payload, quantity : 1, total : total}]
+                        toast('Item added to cart.')
+                    }else{
+                        toast('Already added to cart.')
+                    }
+                    
+                },
         deleteCartItem : (state, action) => {
                 console.log(action.payload);
-                toast('Item deleted')
+                toast('Item removed from cart.')
                 const index = state.cartItem.findIndex(item => item._id === action.payload)
                 state.cartItem.splice(index, 1)
         },
@@ -31,7 +41,9 @@ export const productSlice = createSlice({
         decreaseQuantity : (state, action) => {
             const index = state.cartItem.findIndex(item => item._id === action.payload)
             let quantity = state.cartItem[index].quantity
-            state.cartItem[index].quantity = --quantity
+            if (quantity > 1) {
+                state.cartItem[index].quantity = --quantity
+            }
         }
     }
 })
