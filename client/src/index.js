cc20ebea291e69bdee19715c4dc158e56360d824
Wahome <vinwahome@gmail.com>
2024-02-03 22:13:21 +0300

    Include the cart and menu pages routes

--- client/src/index.js
+++ client/src/index.js
@@ -13,17 +13,19 @@ import Newproduct from './pages/Newproduct';
 import Signup from './pages/Signup';
 import { store } from './redux/index'
 import { Provider } from 'react-redux';
+import Cart from './pages/Cart';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
         <Route index element={<Home />} />
-        <Route path='menu' element={<Menu />} />
+        <Route path='menu/:filterBy' element={<Menu />} />
         <Route path='about' element={<About />} />
         <Route path='contact' element={<Contact />} />
         <Route path='login' element={<Login />} />
         <Route path='newproduct' element={<Newproduct />} />
         <Route path='signup' element={<Signup />} />
+        <Route path='cart' element={<Cart />} />
 
     </Route>
   )
